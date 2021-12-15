a2e96b13be37193a5ad8f117c28a8d17ce2bf79d
Daniel Simonkay <dsimonkay@gmail.com>
2021-12-15 23:25:53 +0000

    Disaster

--- december_15/puzzle1.py
+++ december_15/puzzle1.py
@@ -4,58 +4,108 @@ import sys
 
 
 INPUT_FILE = sys.argv[1] if len(sys.argv) > 1 else "./input.txt"
-NR_OF_STEPS = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+NR_OF_ROWS = None
+NR_OF_COLS = None
 
-def add(register, ch):
-    """
-    Adds a single character to the register (if not already there) and increases its corresponding counter value.
-    """
-    if ch:
-        if ch not in register:
-            register[ch] = 0
+costs = []
+total_costs = []
 
-        register[ch] += 1
+def g(cell):
+    global total_costs
+    (i, j) = cell
+    return total_costs[i][j]
+
+def h(cell):
+    global NR_OF_ROWS, NR_OF_COLS
+    (i, j) = cell
+    return (NR_OF_ROWS - 1 - i) + (NR_OF_COLS - 1 - j)
+
+# # Read the input
+# with open(INPUT_FILE, "r") as infile:
+#     while line := infile.readline().rstrip():
+#         costs_row = [int(x) for _, x in enumerate(line)]
+#         costs.append(costs_row)
+#         total_costs.append([0 for x in costs_row])
+
+costs = [
+    [0, 1, 6, 3, 7, 5, 1, 7, 4, 2],
+    [1, 3, 8, 1, 3, 7, 3, 6, 7, 2],
+    [2, 1, 3, 6, 5, 1, 1, 3, 2, 8],
+    [3, 6, 9, 4, 9, 3, 1, 5, 6, 9],
+    [7, 4, 6, 3, 4, 1, 7, 1, 1, 1],
+    [1, 3, 1, 9, 1, 2, 8, 1, 3, 7],
+    [1, 3, 5, 9, 9, 1, 2, 4, 2, 1],
+    [3, 1, 2, 5, 4, 2, 1, 6, 3, 9],
+    [1, 2, 9, 3, 1, 3, 8, 5, 2, 1],
+    [2, 3, 1, 1, 9, 4, 4, 5, 8, 1]]
+
+total_costs = [[0 for x in row] for row in costs]
+
+NR_OF_ROWS = len(costs)
+NR_OF_COLS = len(costs[0])
+TARGET = (NR_OF_ROWS - 1, NR_OF_COLS - 1)
+
+# Easy hack: getting to the top right corner does not cost anything
+costs[0][0] = 0
+total_costs[0][1] = costs[0][1]
+total_costs[1][0] = costs[1][0]
 
 
-# Read the input and prepare the variable holding the data
-polymer = None  # The ever-growing polymer as a simple string
-rules = {}
-with open(INPUT_FILE, "r") as infile:
+total_path_cost = None
+discovered_cells = [(0, 0)]
+frontier = [(0, 1), (1, 0)]
+log = []
 
-    polymer = infile.readline().rstrip()
-    infile.readline()
+for step in range(300):
 
-    # Read the pair insertion rules
-    while line := infile.readline().rstrip():
-        rule = line.split(" -> ")
-        rules[rule[0]] = rule[1]
+    print(f"\n--------------------------\nstep #{step}\nfrontier: {frontier}")
+    min_cost = 999999
+    picked_cell = None
+    for cell in frontier:
+        print(f"  cell({cell[0]}, {cell[1]}) -- g: {g(cell)}   h: {h(cell)}   g+h: {g(cell) + h(cell)}")
+        if g(cell) + h(cell) < min_cost:
+            picked_cell = cell
+            min_cost = g(cell) + h(cell)
 
-assert polymer
-assert rules
+    (row, col) = picked_cell
+    log.append((picked_cell, costs[row][col]))
+    cell_cost = total_costs[row][col]
 
-# Do the processing
-register = {}
-for _ in range(NR_OF_STEPS):
-    register = {}
-    new_polymer = ""
+    print("elem to pick from frontier:", picked_cell, "  cell cost:", cell_cost)
+    if picked_cell == TARGET:
+        print("Victory!")
+        total_path_cost = cell_cost
+        break
 
-    for i in range(len(polymer) - 1):
-        pair = polymer[i: i + 2]
-        to_insert = rules[pair] if pair in rules else ""
-        add(register, pair[0])
-        add(register, to_insert)
+    # Investigate possible candidates
+    for (i, j) in [(row - 1, col), (row, col - 1), (row, col + 1), (row + 1, col)]:
+        print(f"    checking candidate ({i}, {j})...", end="")
+        # or (i, j) in frontier
+        if i < 0 or i == NR_OF_ROWS or j < 0 or j == NR_OF_COLS or (i, j) in discovered_cells:
+            print(" - not passed")
+            continue
 
-        # This is the step where the ineffectiveness comes from: growing the string
-        new_polymer += pair[0] + to_insert
+        print("OK")
+        # Total cost of reaching the candidate
+        total_cost = cell_cost + costs[i][j]
+        frontier.append((i, j))
+        total_costs[i][j] = total_cost
 
-    # Extrawurst for the last character
-    last_char = polymer[-1]
-    add(register, last_char)
-    polymer = new_polymer + last_char
+    frontier.remove(picked_cell)
+    discovered_cells.append(picked_cell)
 
-# Calculate the posterior
-frequencies = [register[ch] for ch in register]
-frequencies.sort()
-result = frequencies[-1] - frequencies[0]
 
-print(f"\nBlablabla ... result after {NR_OF_STEPS} steps: {result}")
+print()
+vis = [["." for x in row] for row in costs]
+for i, ((row, col), cost) in enumerate(log):
+    # print(f"#{i+1}: {log_entry}")
+    vis[row][col] = str(cost)
+
+for i, row in enumerate(vis):
+    for j, value in enumerate(row):
+        print(value, end="")
+    print()        
+
+print(costs)
+
+print(f"\nTotal lowest risk level on the shortest path: {total_cost}")
