2f4d16368f733729751cfb808008891ac26e3a7b
Daniel Simonkay <dsimonkay@gmail.com>
2021-12-19 02:06:14 +0000

    Add puzzle 2 solution for december 15

--- december_15/puzzle2.py
+++ december_15/puzzle2.py
@@ -32,48 +32,6 @@ def multiply_input(input, times):
     return input_copy
 
 
-def g(cell):
-    """
-    Calculate the total cost value for a given cell
-    """
-    global total_costs
-    (i, j) = cell
-
-    return total_costs[i][j]
-
-
-def h(cell):
-    """
-    Calculate the heuristic value for a given cell
-    """
-    global NR_OF_ROWS, NR_OF_COLS
-    (i, j) = cell
-
-    return (NR_OF_ROWS - 1 - i) + (NR_OF_COLS - 1 - j)
-
-
-def add_to_frontier(frontier, from_cell, to_cell):
-    """
-    Add a candidate to the frontier
-    """
-    global total_costs, costs
-    assert from_cell in frontier
-
-    (from_i, from_j) = from_cell
-    (to_i, to_j) = to_cell
-
-    frontier.append(to_cell)
-    total_costs[to_i][to_j] = total_costs[from_i][from_j] + costs[to_i][to_j]
-
-
-def remove_from_frontier(frontier, cell, discovered_cells):
-    """
-    Removes a cell from the frontier and adds it to the discovered cells
-    """
-    frontier.remove(cell)
-    discovered_cells.append(cell)
-
-
 # Read the input
 with open(INPUT_FILE, "r") as infile:
     while line := infile.readline().rstrip():
@@ -83,7 +41,7 @@ with open(INPUT_FILE, "r") as infile:
 # Blow up the input
 costs = multiply_input(costs, 5)
 
-total_costs = [[0 for x in row] for row in costs]
+total_costs = [[0 for _ in row] for row in costs]
 
 NR_OF_ROWS = len(costs)
 NR_OF_COLS = len(costs[0])
@@ -93,40 +51,55 @@ GOAL = (NR_OF_ROWS - 1, NR_OF_COLS - 1)
 # Easy hack: getting to the top right corner does not cost anything
 costs[0][0] = 0
 
-# The first two cells we add to the frontier are hardcoded
-discovered_cells = []
+explored_cells = []
 frontier = [START]
 
-step = 0
-total_cells = NR_OF_ROWS * NR_OF_COLS
+C = 100 / (NR_OF_ROWS * NR_OF_COLS)
 
-while not GOAL in frontier:
-    step += 1
+while frontier:
     elapsed = time.time() - START_TIME
-    print(f"[Step #{step}] Elapsed time: {elapsed:.2f} seconds   Map discovered: {(len(discovered_cells) + len(frontier)) * 100 / total_cells} %    ", end="\r")
+    map_discovered_percent = (len(explored_cells) + len(frontier)) * C
+    print(f"Elapsed time: {int(elapsed / 60)} minutes {(elapsed % 60):.2f} seconds   Map discovered: {map_discovered_percent:.2f} %   ", end="\r")
 
     min_cost_estimate = 999999
     picked_cell = None
     for cell in frontier:
-        cost_estimate = g(cell) + h(cell)
+        (i, j) = cell
+        # g(cell) = total_costs[i][j]
+        # h(cell) = (NR_OF_ROWS - 1 - i) + (NR_OF_COLS - 1 - j), but we don't need the constant parts
+        # as this expression will be calculated for each cell equally and only to compare it against other cells
+        cost_estimate = total_costs[i][j] - i - j
         if cost_estimate < min_cost_estimate:
             picked_cell = cell
             min_cost_estimate = cost_estimate
 
-    assert picked_cell
     (row, col) = picked_cell
+
+    # Exit condition
+    if picked_cell == GOAL:
+        print("\nFound", GOAL)
+        break
 
     # Investigate possible candidates to visit from the picked cell
     for candidate in [(row - 1, col), (row, col - 1), (row, col + 1), (row + 1, col)]:
         (i, j) = candidate
-        if i < 0 or i == NR_OF_ROWS or j < 0 or j == NR_OF_COLS or candidate in discovered_cells or candidate in frontier:
+        if i < 0 or i == NR_OF_ROWS or j < 0 or j == NR_OF_COLS or candidate in explored_cells:
             continue
 
+        # picked cell total cost + candidate cell entering cost
+        candidate_total_cost = total_costs[row][col] + costs[i][j]
+
         # We can finally add the cell to the frontier
-        add_to_frontier(frontier, picked_cell, candidate)
+        if candidate not in frontier:
+            frontier.append(candidate)
+
+        # Register the cost (or adjust it in case we've just found a cheaper way to the candidate)
+        if total_costs[i][j] == 0 or total_costs[i][j] > candidate_total_cost:
+            total_costs[i][j] = candidate_total_cost
 
     # Remove the processed cell from the frontier and add it to the discovered ones
-    remove_from_frontier(frontier, picked_cell, discovered_cells)
+    frontier.remove(picked_cell)
+    explored_cells.append(picked_cell)
 
 
 print(f"\nTotal lowest risk level on the shortest path: {total_costs[NR_OF_ROWS - 1][NR_OF_COLS - 1]}")
