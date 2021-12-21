6fe76f8cb15bc8a9ec412d7c867d321724798be2
Daniel Simonkay <dsimonkay@gmail.com>
2021-12-21 23:22:06 +0100

    Add solution for puzzle 1, December 15 + add skeleton code for puzzle 2 (=copied puzzle1)

--- december_16/puzzle2.py
+++ december_16/puzzle2.py
@@ -8,7 +8,7 @@ LAST_LITERAL_MARKER = "0"
 TYPE_ID_LITERAL = 4
 
 
-def parse_literal(input):
+def parse_literal(input, start_pos = 0):
     """
     Parses a literal from the input stream
     """
@@ -20,10 +20,11 @@ def parse_literal(input):
         if input[ch_count - 5] == LAST_LITERAL_MARKER:
             break
 
+    print(f"{' ' * start_pos}{input[:ch_count]}")
     return (int(literal, 2), ch_count)
 
 
-def parse_packet(input, stack, packets_to_read = 1):
+def parse_packet(input, stack, packets_to_read = 1, start_pos = 0):
     """
     Parses an input bitstream (represented as a string).
     Returns the number of characters consumed from the input.
@@ -34,29 +35,34 @@ def parse_packet(input, stack, packets_to_read = 1):
 
         version = int(input[ch_count: ch_count + 3], 2)
         type_id = int(input[ch_count + 3: ch_count + 6], 2)
+        print(f"{' ' * (start_pos + ch_count)}{input[ch_count: ch_count + 3]}")
+        print(f"{' ' * (start_pos + ch_count)}---{input[ch_count + 3: ch_count + 6]}")
 
         stack_element = {"version": version, "type_id": type_id}
         stack.append(stack_element)
 
         if type_id == TYPE_ID_LITERAL:
-            (literal, literal_ch_count) = parse_literal(input[ch_count + 6:])
+            (literal, literal_ch_count) = parse_literal(input[ch_count + 6:], start_pos + ch_count + 6)
             stack_element["literal"] = literal
             ch_count += 6 + literal_ch_count
 
         else:
             length_type_id = input[ch_count + 6]
+            print(f"{' ' * (start_pos + ch_count + 6)}{length_type_id}")
             subpacket_chars = 0
             length_field_chars = 0
             if length_type_id == "0":
                 length_field_chars = 15
                 subpacket_length = int(input[ch_count + 7: ch_count + 7 + length_field_chars], 2)
-                subpacket_chars = parse_packet(input[ch_count + 22: ch_count + 22 + subpacket_length], stack, 999)
+                print(f"{' ' * (start_pos + ch_count + 7)}{input[ch_count + 7: ch_count + 22]}")
+                subpacket_chars = parse_packet(input[ch_count + 22: ch_count + 22 + subpacket_length], stack, 999, start_pos + ch_count + 22)
                 assert subpacket_chars == subpacket_length
 
             else:
                 length_field_chars = 11
                 nr_of_packets_to_read = int(input[ch_count + 7: ch_count + 7 + length_field_chars], 2)
-                subpacket_chars = parse_packet(input[ch_count + 18:], stack, nr_of_packets_to_read)
+                print(f"{' ' * (start_pos + ch_count + 7)}{input[ch_count + 7: ch_count + 18]}")
+                subpacket_chars = parse_packet(input[ch_count + 18:], stack, nr_of_packets_to_read, start_pos + ch_count + 18)
 
             ch_count += 7 + length_field_chars + subpacket_chars
 
@@ -70,10 +76,18 @@ input = ""
 with open(INPUT_FILE, "r") as infile:
     line = infile.readline().rstrip()
     for _, ch in enumerate(line):
+        print(f"   {ch}", end="")
         input += bin(int(ch, 16))[2:].zfill(4)
+    print()
 
+print(input)
 stack = []
+
 parse_packet(input, stack)
+
+for elem in stack:
+    print(elem)
+
 sum_of_version_numbers = sum(x["version"] for x in stack)
 
 print(f"\nSum of the version numbers in the input: {sum_of_version_numbers}")
